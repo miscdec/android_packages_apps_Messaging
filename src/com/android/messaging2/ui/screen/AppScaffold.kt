775c46dcef199d1e9279fb6e5959f8241923c1d4
miscdec <70028744+miscdec@users.noreply.github.com>
2023-10-06 17:12:08 +0800

    Messaging: Supplement compose dependencies
    Signed-off-by: miscdec <70028744+miscdec@users.noreply.github.com>

---
+++ src/com/android/messaging2/ui/screen/AppScaffold.kt
@@ -0,0 +1,69 @@
+package com.android.messaging2.ui.screen
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScaffoldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.contentColorFor
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import com.android.messaging.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppScaffold(
+    modifier: Modifier = Modifier,
+    title: String = "",
+    onBack: () -> Unit = {},
+    actions: @Composable() (RowScope.() -> Unit) = {},
+    navigationIcon: @Composable () -> Unit = {
+        IconButton(onClick = onBack) {
+            Icon(
+                imageVector = Icons.Default.ArrowBack,
+                contentDescription = stringResource(id = R.string.back)
+            )
+        }
+    },
+    topBar: @Composable () -> Unit = {
+        TopAppBar(
+            title = { Text(text = title) },
+            navigationIcon = navigationIcon,
+            actions = actions
+        )
+    },
+    bottomBar: @Composable () -> Unit = {
+
+    },
+    snackbarHost: @Composable () -> Unit = {},
+    floatingActionButton: @Composable () -> Unit = {},
+    floatingActionButtonPosition: FabPosition = FabPosition.End,
+    containerColor: Color = MaterialTheme.colorScheme.background,
+    contentColor: Color = contentColorFor(containerColor),
+    contentWindowInsets: WindowInsets = ScaffoldDefaults.contentWindowInsets,
+    content: @Composable (PaddingValues) -> Unit
+) {
+    return Scaffold(
+        modifier = modifier,
+        topBar = topBar,
+        bottomBar = bottomBar,
+        snackbarHost = snackbarHost,
+        floatingActionButton = floatingActionButton,
+        floatingActionButtonPosition = floatingActionButtonPosition,
+        containerColor = containerColor,
+        contentColor = contentColor,
+        contentWindowInsets = contentWindowInsets,
+        content = content
+    )
+}
